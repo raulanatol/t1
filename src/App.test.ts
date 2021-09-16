864c81144f5306bda5f39775627eea642f4b47bc
raulanatol <raul@natol.es>
2021-09-16 17:16:09 +0100

    Initial commit 🤘

---
+++ src/App.test.ts
@@ -0,0 +1,7 @@
+import { t1 } from './App';
+
+describe('t1', () => {
+  it('should returns 2', () => {
+    expect(t1('')).toBe(0);
+  });
+});
---
+++ src/App.ts
@@ -0,0 +1,4 @@
+export const t1 = (value: string): number => {
+  console.log('Value is: ', value);
+  return 0;
+}
